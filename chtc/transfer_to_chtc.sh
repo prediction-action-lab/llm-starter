f8a657d44ac04f9cf2072fcbab68bb5c5f7667b2
Patron <Patron@Patrons-MacBook-Air.local>
2026-02-22 10:40:44 -0600

    Update transfer file

--- chtc/transfer_to_chtc.sh
+++ chtc/transfer_to_chtc.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 source .env
 
 f=llm-starter
 
 cd ../.. # cd just outside the repo
+
+# 1. Create the tarball
 tar --exclude='.git' \
     --exclude='.idea'  \
     -czvf ${f}.tar.gz $f
@@ -10,7 +13,18 @@ tar --exclude='.git' \
 USER=${CHTC_USER}
 HOSTNAME="ap2001.chtc.wisc.edu"
 
-echo "Transferring to CHTC: ${USER} to ${HOSTNAME}"
+echo "============================================"
+echo "1. Transferring tarball to CHTC Staging..."
+echo "============================================"
+scp ${f}.tar.gz ${USER}@${HOSTNAME}:/staging/${USER}/
 
-scp ${f}.tar.gz ${USER}@${HOSTNAME}:/staging/${USER}
+echo "============================================"
+echo "2. Syncing code to CHTC Home directory..."
+echo "============================================"
+# This safely mirrors your local code (including .env) to your CHTC home directory
+rsync -avz --exclude='.git' --exclude='.idea' $f ${USER}@${HOSTNAME}:~/
+
+# Clean up local tarball
 rm ${f}.tar.gz
+
+echo "Done! You can now run ./login_chtc.sh"
